da156383fa9e8845d421b136e32a6babfce46075
llpds <llpds@mail.ru>
2023-07-30 19:18:51 +0300

    3.15: Phonebook database, step3

--- index.js
+++ index.js
@@ -47,9 +47,9 @@ app.post ('/api/persons', (request, response) => {
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(p => p.id !== id)
-  response.status(204).end()
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => response.status(204).end())
+    .catch(error => next(error))
 })
 
 const PORT = process.env.PORT
